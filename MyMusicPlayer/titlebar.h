9899de8719638a3bbb7076433b0d456d8d411403
luzy99 <1694957926@qq.com>
2019-08-24 19:20:43 +0800

    first commit

---
+++ MyMusicPlayer/titlebar.h
@@ -0,0 +1,17 @@
+#ifndef TITLEBAR_H
+#define TITLEBAR_H
+
+#include <QWidget>
+
+class TitleBar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TitleBar(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // TITLEBAR_H
