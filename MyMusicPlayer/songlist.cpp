4ab2e461dbac3794e7d1d51d457603bb8b5de6bb
YulinZhang6 <46994840+YulinZhang6@users.noreply.github.com>
2019-08-28 08:50:46 +0800

    Update songlist.cpp

--- MyMusicPlayer/songlist.cpp
+++ MyMusicPlayer/songlist.cpp
@@ -9,32 +9,67 @@ SongList::SongList(QWidget *parent) : QWidget(parent)
     scrollLists = new QScrollArea (g_container);
     scrollListsWidget = new QWidget(scrollLists);
     scrollSongsWidget = new QWidget (scrollSongs);
-    listList = new QListWidget(scrollSongsWidget);
-    listSongs = new QListWidget(scrollListsWidget);
+    listList = new QListWidget(scrollListsWidget);
+    listSongs = new QListWidget(scrollSongsWidget);
+    labelLists = new QLabel(scrollListsWidget);
+    labelSongs = new QLabel(scrollSongsWidget);
 
+    lovedMusiclist = new QListWidgetItem(listList);
+
+    createMusicListButton = new QPushButton(scrollListsWidget);
+
+    //滚轮属性
     scrollLists->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    scrollSongs->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    scrollSongs->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     //各个窗口位置确认
     g_container->setGeometry(0, 0, 240 , 870);
     scrollLists->setGeometry(0, 0, 240, 290);
     scrollSongs->setGeometry(0, 290, 240, 580);
 
-    scrollListsWidget->setGeometry(0, 0, 230, 0);
-    scrollListsWidget->setMinimumSize(230, 0);
+    scrollListsWidget->setGeometry(0, 0, 230, 290);
+    scrollListsWidget->setMinimumSize(230, 290);
 
-    scrollSongsWidget->setGeometry(0, 0, 230 , 0);
-    scrollSongsWidget->setMinimumSize(230, 0);
-    listList->setGeometry(0, 0, 230, 0);
-    listSongs->setGeometry(0, 0, 230, 0);
+    scrollSongsWidget->setGeometry(0, 0, 230 , 580);
+    scrollSongsWidget->setMinimumSize(230, 580);
 
-    //让部件显示
-    g_container->show();
-    scrollLists->show();
-    scrollSongs->show();
-    scrollListsWidget->show();
-    scrollSongsWidget->show();
-    listList->show();
-    listSongs->show();
+    labelLists->setGeometry(0, 0 , 230 ,30);
+    labelSongs->setGeometry(0, 0, 230, 30);
+
+    listList->setGeometry(0, 30, 230, 260);
+    listSongs->setGeometry(0, 30, 230, 550);
+
+    createMusicListButton->setGeometry(200, 0, 30, 30);
+    //设置两个滚轮区无边框
+    scrollLists->setFrameShape(QFrame::NoFrame);
+    scrollSongs->setFrameShape(QFrame::NoFrame);
+
+    //消除滚轮区内部的窗口边框
+    listList->setFrameShape(QFrame::NoFrame);
+    listSongs->setFrameShape(QFrame::NoFrame);
+
+    //标签文字
+    labelLists->setText(" 我的歌单");
+    labelSongs->setText(" 播放列表");
+    labelLists->setStyleSheet("color:rgb(139, 139, 139);");
+    labelSongs->setStyleSheet("color:rgb(139, 139, 139);");
+
+    //列表项的定义，样式
+    lovedMusiclist->setIcon(QIcon(":/icon/res/favourite_d.ico"));
+    lovedMusiclist->setText("我喜欢的音乐");
+    QFont font = QFont("宋体",10,QFont::Bold);
+    lovedMusiclist->setFont(font);
+
+    //新建歌单按钮样式
+    createMusicListButton->setFlat(true);
+    createMusicListButton->setIcon(QIcon(":/icon/res/add.png"));
+//    createMusicListButton->setStyleSheet("QPushButton {"
+//                                             "border: 0px solid #8f8f91;"
+//                                             "border-radius:15px;"
+//                                             "background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
+//                                             "stop: 0 #f6f7fa, stop: 1 #dadbde);"
+//                                             "min-width: 10px;"
+//                                         "}");
+
 
     //设置滚轮样式
     scrollLists->setWidget(scrollListsWidget);
@@ -245,10 +280,14 @@ SongList::SongList(QWidget *parent) : QWidget(parent)
         "border-radius:4px;"
     "}");
 
-    //设置两个滚轮区无边框
-    scrollLists->setFrameShape(QFrame::NoFrame);
-    scrollSongs->setFrameShape(QFrame::NoFrame);
-
+    //让部件显示
+    g_container->show();
+    scrollLists->show();
+    scrollSongs->show();
+    scrollListsWidget->show();
+    scrollSongsWidget->show();
+    listList->show();
+    listSongs->show();
 }
 
 
@@ -265,4 +304,17 @@ void SongList::resetGeometry()
     scrollSongs->setGeometry(0, ((sizeY-30 ))/3, 240, (2*(sizeY-30))/3);
 }
 
+//void SongList::onListSongsDoubleClicked(const QModelIndex &index)
+//{
+//    int curRow = index.row();
+//    emit playMusic(curRow);
+//}
+
+void SongList::onChangeListSongs(QString fileName, int behaviorIndex)
+{
+    if(behaviorIndex == 1)
+    {
+        listSongs->addItem(fileName);
+    }
+}
 
