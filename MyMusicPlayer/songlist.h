07f50162af4fba52bcfc9a2de6d2f1b3d864eef0
YulinZhang6 <46994840+YulinZhang6@users.noreply.github.com>
2019-08-28 08:51:48 +0800

    Update songlist.h

--- MyMusicPlayer/songlist.h
+++ MyMusicPlayer/songlist.h
@@ -6,15 +6,24 @@
 #include <QListWidget>
 #include<QGroupBox>
 #include <QScrollArea>
+#include<QLabel>
+#include<QPushButton>
 class SongList : public QWidget
 {
     Q_OBJECT
 public:
     explicit SongList(QWidget *parent = nullptr);
 
+signals:
+    void playMusic(int SongIndex); //触发播放音乐
 
 public slots:
     void resetGeometry();
+    //void onListSongsDoubleClicked(const QModelIndex &index); //双击歌曲项时的槽函数
+    void onChangeListSongs(QString fileName,int behaviorIndex); //处理changeListSongs信号的槽函数
+
+
+
 
 private:
     QGroupBox *g_container;
@@ -24,6 +33,11 @@ private:
     QWidget *scrollSongsWidget;
     QListWidget *listList;
     QListWidget *listSongs;
+    QLabel *labelLists;
+    QLabel *labelSongs;
+    QListWidgetItem *lovedMusiclist;
+    QListWidgetItem *createdMusicList;
+    QPushButton *createMusicListButton;
 };
 
 #endif // SONGLIST_H
