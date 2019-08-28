3b187890ed9fbd7f0b73bd89b4ccac56b91db2be
YulinZhang6 <46994840+YulinZhang6@users.noreply.github.com>
2019-08-28 08:47:55 +0800

    Update musicplaybar.cpp

--- MyMusicPlayer/musicplaybar.cpp
+++ MyMusicPlayer/musicplaybar.cpp
@@ -1,32 +1,36 @@
 #include "musicplaybar.h"
-#include <QHBoxLayout>
+#include<QDebug>
 #include <QPalette>
-
 MusicPlayBar::MusicPlayBar(QWidget *parent)
     : QWidget(parent)
-    ,themeColor(205,10,10)
 {
-    QHBoxLayout *layout = new QHBoxLayout; //采用水平布局
+    layout = new QHBoxLayout(parent); //采用水平布局
 
     //初始化上一首按钮
     previousBtn = new QPushButton;
     previousBtn->setIcon(QIcon(":/icon/res/previous.png"));
-    previousBtn->setIconSize(QSize(50,50));
+    previousBtn->setIconSize(QSize(35,35));
+    previousBtn->setFixedSize(QSize(50,50));
     previousBtn->setFlat(true);
+    previousBtn->setToolTip("上一首");
     layout->addWidget(previousBtn);
 
     //初始化播放按钮
     playBtn = new QPushButton;
     playBtn->setIcon(QIcon(":/icon/res/play.png"));
-    playBtn->setIconSize(QSize(50,50));
+    playBtn->setIconSize(QSize(45,45));
+    playBtn->setFixedSize(QSize(50,50));
     playBtn->setFlat(true);
+    playBtn->setToolTip("播放");
     layout->addWidget(playBtn);
 
     //初始化下一首按钮
     nextBtn = new QPushButton;
     nextBtn->setIcon(QIcon(":/icon/res/next.png"));
-    nextBtn->setIconSize(QSize(50,50));
+    nextBtn->setIconSize(QSize(35,35));
+    nextBtn->setFixedSize(QSize(50,50));
     nextBtn->setFlat(true);
+    nextBtn->setToolTip("下一首");
     layout->addWidget(nextBtn);
 
     //初始化音乐播放的当前时间
@@ -48,7 +52,8 @@ MusicPlayBar::MusicPlayBar(QWidget *parent)
     //初始化静音按钮
     muteBtn = new QPushButton;
     muteBtn->setIcon(QIcon(":/icon/res/soundFull.png"));
-    muteBtn->setIconSize(QSize(30,30));
+    muteBtn->setIconSize(QSize(25,25));
+    muteBtn->setFixedSize(QSize(25,25));
     muteBtn->setFlat(true);
     layout->addWidget(muteBtn);
 
@@ -62,48 +67,82 @@ MusicPlayBar::MusicPlayBar(QWidget *parent)
 
     //初始化播放模式按钮
     playModeBtn = new QPushButton;
-    playModeBtn->setIcon(QIcon(":/icon/res/modeLoop.png"));
-    playModeBtn->setIconSize(QSize(50,50));
+    playModeBtn->setIcon(QIcon(":/icon/res/modeSequence.png"));
+    playModeBtn->setIconSize(QSize(25,25));
+    playModeBtn->setFixedSize(QSize(25,25));
     playModeBtn->setFlat(true);
+    playModeBtn->setToolTip("列表循环");
     layout->addWidget(playModeBtn);
 
     //显示歌词按钮
     showLyricsBtn = new QPushButton;
     showLyricsBtn->setIcon(QIcon(":/icon/res/lyrics.png"));
-    showLyricsBtn->setIconSize(QSize(50,50));
+    showLyricsBtn->setIconSize(QSize(35,35));
+    showLyricsBtn->setFixedSize(QSize(35,35));
     showLyricsBtn->setFlat(true);
+    showLyricsBtn->setToolTip("点击显示/隐藏歌词");
     layout->addWidget(showLyricsBtn);
 
     //显示倍速选择
-    playSpeed = new QPushButton;
-    playSpeed->setIcon(QIcon(":/icon/res/rate100.png"));
-    playSpeed->setIconSize(QSize(50,50));
-    playSpeed->setFlat(true);
-    layout->addWidget(playSpeed);
+    playSpeedBtn = new QPushButton;
+    playSpeedBtn->setIcon(QIcon(":/icon/res/rate100.png"));
+    playSpeedBtn->setIconSize(QSize(35,35));
+    playSpeedBtn->setFixedSize(QSize(35,35));
+    playSpeedBtn->setFlat(true);
+    playSpeedBtn->setToolTip("点击切换播放速度");
+    layout->addWidget(playSpeedBtn);
+    currentSpeed = 1.0;
 
     //将写的布局添加进当前栏
     this->setLayout(layout);
-
-    //修改当前组件的颜色
-    QPalette pt = this->palette();
-    pt.setColor(QPalette::Window,themeColor);
-    this->setPalette(pt);
-
     //初始化播放器和播放列表
     player = new QMediaPlayer(this);
     playlist = new QMediaPlaylist(this);
     playlist->setPlaybackMode(QMediaPlaylist::Loop); //循环模式
     player->setPlaylist(playlist);
 
-    connect(player,SIGNAL(stateChanged(QMediaPlayer::State)),
-            this,SLOT(onStateChanged(QMediaPlayer::State)));
+    //连接信号与槽
+    initSignalsAndSlots();
+
+
+}
+
+void MusicPlayBar::initSignalsAndSlots()
+{
+    //播放位置改变
     connect(player,SIGNAL(positionChanged(qint64)),
             this,SLOT(onPositionChanged(qint64)));
+    //切歌
     connect(player,SIGNAL(durationChanged(qint64)),
             this,SLOT(onDurationChanged(qint64)));
-    connect(playlist,SIGNAL(currentIndexChanged(int)),
-            this,SLOT(onPlaylistChanged(int)));
+    //"上一首"按钮
+    connect(previousBtn,SIGNAL(clicked()),
+            this,SLOT(on_previousBtn_clicked()));
+    //”播放&暂停”按钮
+    connect(playBtn,SIGNAL(clicked()),
+            this,SLOT(on_playBtn_clicked()));
+    //"下一首"按钮
+    connect(nextBtn,SIGNAL(clicked()),
+           this,SLOT(on_nextBtn_clicked()));
+    //拖动播放进度条:
+    connect(playSlider,SIGNAL(sliderReleased()),
+            this,SLOT(on_playSlider_sliderReleased()));
+    //"静音"按钮
+    connect(muteBtn,SIGNAL(clicked()),
+            this,SLOT(on_muteBtn_clicked()));
+    //拖动音量条:
+    connect(soundSlider,SIGNAL(valueChanged(int)),
+            this,SLOT(on_soundSlider_valueChanged(int)));
+    //"播放模式"按钮
+    connect(playModeBtn,SIGNAL(clicked()),
+            this,SLOT(on_playModeBtn_clicked()));
+    //"倍速"按钮
+    connect(playSpeedBtn,SIGNAL(clicked()),
+            this,SLOT(on_playSpeedBtn_clicked()));
 }
+
+
+
 
 //在播放器播放状态变化时发射，以更新界面上按钮的使能状态
 void MusicPlayBar::onStateChanged(QMediaPlayer::State state)
@@ -113,11 +152,15 @@ void MusicPlayBar::onStateChanged(QMediaPlayer::State state)
    if(isPlaying)
    {
        playBtn->setIcon(QIcon(":/icon/res/play.png"));
+       playBtn->setToolTip("播放");
    }
-   else {
+   else
+   {
        playBtn->setIcon(QIcon(":/icon/res/pause.png"));
+       playBtn->setToolTip("暂停");
    }
 }
+
 
 //在文件时长变化时发射，用于跟新用户界面上文件时间长度的显示
 void MusicPlayBar::onDurationChanged(qint64 duration)
@@ -127,16 +170,7 @@ void MusicPlayBar::onDurationChanged(qint64 duration)
        int secs = int(duration)/1000; //秒
        int mins =  secs/60; //分钟
        secs = secs%60; //余秒数
-       QString minStr(mins);
-       QString secStr;
-       secStr = QString(secs);
-       if(secs>10) ;
-       else
-       {
-           secStr = "0"+secStr;
-       }
-
-       durationTime = minStr+":"+secStr;
+       durationTime = QString::asprintf("%d:%d",mins,secs);
        totalTimeLabel->setText(durationTime);
 }
 
@@ -144,27 +178,175 @@ void MusicPlayBar::onDurationChanged(qint64 duration)
 void MusicPlayBar::onPositionChanged(qint64 position)
 {
     //当前文件播放进度变化，更新进度显示
-       if(playSlider->isSliderDown())
-           return; //手动状态，不处理
        playSlider->setSliderPosition(int(position));
        int secs = int(position)/1000; //秒
        int mins =  secs/60; //分钟
        secs = secs%60; //余秒数
-       QString minStr(mins);
-       QString secStr;
-       secStr = QString(secs);
-       if(secs>10) ;
-       else
-       {
-           secStr = "0"+secStr;
-       }
-
-       positionTime = minStr+":"+secStr;
+       positionTime = QString::asprintf("%d:%d",mins,secs);
        currentTimeLabel->setText(positionTime);
 }
+
+void MusicPlayBar::onChangePlaylist(QUrl url, int behaviorIndex)
+{
+    if(behaviorIndex == 1)
+    {
+        playlist->addMedia(url);
+    }
+}
+
+void MusicPlayBar::onPlayMusic(int SongIndex)
+{
+        playlist->setCurrentIndex(SongIndex);
+        player->play();
+        playBtn->setIcon(QIcon(":/icon/res/pause.png"));
+}
+
+//点击"上一首"触发的槽函数
+void MusicPlayBar::on_previousBtn_clicked()
+{
+    int curIndex = playlist->currentIndex();
+    playlist->setCurrentIndex(curIndex-1);
+    player->play();
+}
+
+//点击”播放&暂停”触发的槽函数
+void MusicPlayBar::on_playBtn_clicked()
+{
+    //播放
+    if(player->state() == QMediaPlayer::PlayingState )
+    {
+        player->pause();
+        playBtn->setIcon(QIcon(":/icon/res/play.png"));
+        playBtn->setToolTip("播放");
+    }
+    else
+    {
+        if(playlist->currentIndex()<0)
+        {
+            playlist->setCurrentIndex(0);
+        }
+        player->play();
+        playBtn->setIcon(QIcon(":/icon/res/pause.png"));
+        playBtn->setToolTip("暂停");
+    }
+}
+
+//点击"下一首"触发的槽函数
+void MusicPlayBar::on_nextBtn_clicked()
+{
+    int curIndex = playlist->currentIndex();
+    playlist->setCurrentIndex(curIndex+1);
+    player->play();
+}
+
+//用户拖动进度条之后的槽函数
+void MusicPlayBar::on_playSlider_sliderReleased()
+{
+    int newValue = playSlider->value();
+    player->setPosition(newValue);
+}
+
+//点击"静音"触发的槽函数
+void MusicPlayBar::on_muteBtn_clicked()
+{
+    //静音控制
+    bool mute = player->isMuted();
+    player->setMuted(!mute);
+    if(mute)
+    {
+        muteBtn->setIcon(QIcon(":/icon/res/soundNormal.png"));
+    }
+    else
+    {
+        muteBtn->setIcon(QIcon(":/icon/res/soundMute.png"));
+    }
+}
+
+//歌曲滑条被拖动时触发的槽函数
+void MusicPlayBar::on_soundSlider_valueChanged(int value)
+{
+    //更改图标
+    if(value == 0)
+    {
+        muteBtn->setIcon(QIcon(":/icon/res/soundMute.png"));
+    }
+    else if(value == 99)
+    {
+        muteBtn->setIcon(QIcon(":/icon/res/soundFull.png"));
+    }
+    else
+    {
+        muteBtn->setIcon(QIcon(":/icon/res/soundNormal.png"));
+    }
+
+    player->setVolume(value);
+}
+
+//切换播放模式时触发的槽函数
+void MusicPlayBar::on_playModeBtn_clicked()
+{
+    int curMode = playlist->playbackMode();
+    switch (curMode)
+    {
+    //如果当前是单曲循环->列表循环
+    case 1:
+        playlist->setPlaybackMode(QMediaPlaylist::Loop);
+        playModeBtn->setIcon(QIcon(":/icon/res/modeSequence.png"));
+        playModeBtn->setToolTip("列表循环");
+        break;
+    //如果当前是列表循环->列表随机
+    case 3:
+        playlist->setPlaybackMode(QMediaPlaylist::Random);
+        playModeBtn->setIcon(QIcon(":/icon/res/modeRandom.png"));
+        playModeBtn->setToolTip("列表随机");
+        break;
+    case 4:
+    //如果当前是列表随机->单曲循环
+        playlist->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+        playModeBtn->setIcon(QIcon(":/icon/res/modeLoop.png"));
+        playModeBtn->setToolTip("单曲循环");
+        break;
+    }
+}
+
+//切换倍数时触发的槽函数
+void MusicPlayBar::on_playSpeedBtn_clicked()
+{
+    if(currentSpeed == 1.0)
+    {
+        //如果当前是1.0x->1.5x
+        currentSpeed = 1.5;
+        player->setPlaybackRate(currentSpeed);
+        playSpeedBtn->setIcon(QIcon(":/icon/res/rate150.png"));
+    }
+    else if(currentSpeed == 1.5)
+    {
+        //如果当前是1.5x->2.0x
+        currentSpeed = 2.0;
+        player->setPlaybackRate(currentSpeed);
+        playSpeedBtn->setIcon(QIcon(":/icon/res/rate200.png"));
+    }
+    else if(currentSpeed == 2.0)
+    {
+        //如果当前是2.0x->0.5x
+        currentSpeed = 0.5;
+        player->setPlaybackRate(currentSpeed);
+        playSpeedBtn->setIcon(QIcon(":/icon/res/rate050.png"));
+    }
+    else if(currentSpeed == 0.5)
+    {
+        //如果当前是0.5x->1.0x
+        currentSpeed = 1.0;
+        player->setPlaybackRate(currentSpeed);
+        playSpeedBtn->setIcon(QIcon(":/icon/res/rate100.png"));
+    }
+    else {}
+}
+
 
 void MusicPlayBar::changeThemeColor(QColor)
 {
     //改更各个组件的颜色
 }
+
 
